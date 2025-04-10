57e3f840a1a598e40ca740ec9df08295d1c1774e
Dileep Nishad <dileepr143184@gmail.com>
2025-04-10 14:12:16 +0530

    add backend base url

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,3 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react-swc';
-
-// // Vite configuration
-// export default defineConfig({
-//   // Enable React with SWC for faster builds
-//   plugins: [react()],
-
-//   // Expose only variables prefixed with VITE_ to the frontend
-//   envPrefix: 'VITE_',
-
-//   // Dev server configuration
-//   server: {
-//     host: 'localhost',
-//     port: 5173, // Default port
-
-//     proxy: {
-//       // Proxy requests starting with /api to backend
-//       '/api': {
-//         target: 'http://localhost:8000', // Your Express backend
-//         changeOrigin: true,              // Needed for virtual hosts
-//         secure: false,                   // Set to true if using HTTPS
-//         // DO NOT rewrite the path because you are keeping `/api` in backend
-//         // rewrite: (path) => path.replace(/^\/api/, ''), <-- ❌ remove this line
-//       },
-//     },
-//   },
-// });
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
@@ -45,7 +16,7 @@ export default defineConfig({
     proxy: {
       // Proxy requests starting with /api to backend
       '/api': {
-        target: 'http://localhost:8000', // Your Express backend
+        target: "https://real-estate-backend-in-mern.onrender.com", // Your Express backend
         changeOrigin: true,
         secure: false,
       },
